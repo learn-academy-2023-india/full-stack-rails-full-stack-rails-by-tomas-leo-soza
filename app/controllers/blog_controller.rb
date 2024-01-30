7341acec3e393ecbb89e3b354cf4eecf7697fbcb
Mamba240824 <leo.ramos70@yahoo.com>
2024-01-30 14:05:52 -0800

    second commit

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,22 @@
+class BlogController < ApplicationController
+    def index
+        @blog_list = Blog.all 
+    end
+
+    def dogs
+    
+    end
+
+    def cats 
+
+    end
+
+    def snakes
+    
+    end
+
+    def racoons
+
+    end 
+    
+end
